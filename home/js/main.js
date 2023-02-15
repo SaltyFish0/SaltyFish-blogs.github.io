bd51e299813e633751e9717aa0db18a3cb568f4a
咸鱼 <92703848+SaltyFish0@users.noreply.github.com>
2023-02-15 11:47:05 +0800

    2.15
    2.15

---
+++ home/js/main.js
@@ -0,0 +1,22 @@
+// let img = document.querySelector('.img')
+// let deg = 0
+// let y = 0
+// let index = 0
+// window.addEventListener('mousemove', function(xyz) {
+// 	var scrollw = document.body.scrollTop || document.documentElement.scrollTop;
+// 	var scrollh = document.body.scrollLeft || document.documentElement.scrollLeft;
+// 	var x = event.clientX;
+// 	var y = event.clientY;
+// 	img.style.top = y + scrollw + 12 + "px"
+// 	img.style.left = x + scrollh + 12 + "px"
+
+// })
+
+
+
+
+// setInterval(function() {
+
+// 	img.style.transform = "rotate(" + deg + "deg)"
+// 	deg += 20
+// }, 20);
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+console.log("Welcome to SaltyFish's blog 0。o")
